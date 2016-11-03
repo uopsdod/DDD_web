c40288be559b1a411c6af1a7ff066ec4ee05cb3e
uopsdod <uopsdod@gmail.com>
2016-11-03 13:05:32 +0800

    新增共住至DB取得圖片: 新增ImageUtil.java - 縮圖工具類別 修改Partner.java - 新增action = "getImage"

--- DDD_web/src/android/live2/Partner.java
+++ DDD_web/src/android/live2/Partner.java
@@ -2,6 +2,7 @@ package android.live2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,17 +22,19 @@ import com.mem.model.MemJNDIDAO;
 import com.mem.model.MemVO;
 
 @SuppressWarnings("serial")
-@WebServlet("/Partner")
+@WebServlet("/live2/Partner")
 public class Partner extends HttpServlet {
 	// private ServletContext context;
 	private final static String CONTENT_TYPE = "text/html; charset=UTF-8";
+	private MemDAO_interface dao_mem;
 	private List<MemVO> memVOList;
+	private static Integer reqCount = 0;
 
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		MemDAO_interface dao_mem = new MemJNDIDAO();
+		dao_mem = new MemJNDIDAO();
 		memVOList = new ArrayList<MemVO>();
 		memVOList = dao_mem.getAll();
 		System.out.println(memVOList);
@@ -39,13 +42,14 @@ public class Partner extends HttpServlet {
 	}
 
 	@Override
-	public void doPost(HttpServletRequest rq, HttpServletResponse rp)
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
-		rq.setCharacterEncoding("UTF-8");
+		reqCount++;
+		req.setCharacterEncoding("UTF-8");
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd HH:mm:ss")
                 .create();
-		BufferedReader br = rq.getReader();
+		BufferedReader br = req.getReader();
 		StringBuilder jsonIn = new StringBuilder();
 		String line = null;
 		while ((line = br.readLine()) != null) {
@@ -53,20 +57,39 @@ public class Partner extends HttpServlet {
 		}
 		JsonObject jsonObject = gson.fromJson(jsonIn.toString(),
 				JsonObject.class);
-		String param = jsonObject.get("param").getAsString();
+		String param = jsonObject.get("action").getAsString();
 		String outStr = "";
-		if (param.equals("partner")) {
-			System.out.println("match");
-			outStr = gson.toJson(memVOList);
-		} else  {
-			System.out.println("does not match");
+		if ("getAll".equals(param)) {
+			System.out.println("getAll match");
+			// 去掉圖片資料，提升效能
+			List<MemVO> tmpMemVOList = this.memVOList;
+			for (MemVO myVO: tmpMemVOList){
+				myVO.setMemProfile(null);
+			}
+			outStr = gson.toJson(tmpMemVOList);
+			res.setContentType(CONTENT_TYPE);
+			PrintWriter out = res.getWriter();
+			System.out.println("outStr:" + outStr);
+			out.println(outStr);
+		}else if("getImage".equals(param)){
+			System.out.println("getImage match");
+			OutputStream os = res.getOutputStream();
+			int memId = jsonObject.get("memId").getAsInt();
+			int imageSize = jsonObject.get("imageSize").getAsInt();
+			byte[] image = dao_mem.findByPrimaryKey(Integer.toString(memId)).getMemProfile();
+			if (image != null) {
+				image = ImageUtil.shrink(image, imageSize);
+				res.setContentType("image/jpeg");
+				res.setContentLength(image.length);
+			}
+			os.write(image);
+		}else  {
+			System.out.println("does not match any keys");
 		}
-
-		rp.setContentType(CONTENT_TYPE);
-		PrintWriter out = rp.getWriter();
-		System.out.println("outStr:" + outStr);
-		out.println(outStr);
+		
+		System.out.println("reqCount+ " + reqCount);
 	}
+	
 
 	@Override
 	public void doGet(HttpServletRequest rq, HttpServletResponse rp)
