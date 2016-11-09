aae917326cd7229f06194a7eb9398c6f702da47b
uopsdod <uopsdod@gmail.com>
2016-11-09 22:24:34 +0800

    共住-繼續修改MsgCenter，建立兩個map收tokenId與session

--- DDD_web/src/android/live2/MsgCenter.java
+++ DDD_web/src/android/live2/MsgCenter.java
@@ -2,6 +2,7 @@ package android.live2;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,14 +19,19 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
-/**
- * Servlet implementation class Chatroom
- */
-@ServerEndpoint("/android/live2/Chatroom")
-public class Chatroom extends HttpServlet {
-
+@ServerEndpoint("/android/live2/MsgCenter")
+public class MsgCenter extends HttpServlet {
+				//  <memId,tokenId>
+	private HashMap<String,String> tokenMap = new HashMap<>();
+				//  <memId,session>	
+	private HashMap<String,Session> sessionMap = new HashMap<>();
 
 	private static final Set<Session> connectedSessions = Collections.synchronizedSet(new HashSet<>());
 
@@ -34,15 +40,37 @@ public class Chatroom extends HttpServlet {
 		connectedSessions.add(userSession);
 		String text = String.format("Session ID = %s, connected", userSession.getId());
 		System.out.println(text);
+		
+		
 	}
 
 	@OnMessage
-	public void onMessage(Session userSession, String message) {
+	public void onMessage(Session aUserSession, String aMessage) throws JSONException {
+		// 客戶端傳來FCM - tockenId
+		Gson gson = new GsonBuilder().create();
+		JSONObject jsonObj = new JSONObject(aMessage);
+		String action = jsonObj.getString("action");
+		String memId = jsonObj.getString("memId");
+		if ("uploadTokenId".equals(action)){
+			System.out.println("jsonObj: " + jsonObj);
+			String token = jsonObj.getString("tokenId");
+			tokenMap.put(memId, token);
+			aUserSession.getAsyncRemote().sendText("Server aleary stored your token.");
+			System.out.println("tokenMap.size(): "+tokenMap.size());
+			return;
+		}
+		
+		// 客戶第一次進行即時通訊(進行中)
+		if (!sessionMap.containsKey(memId)){
+			sessionMap.put(memId, aUserSession);
+		}
+		
+		// 一般聊天通訊
 		for (Session session : connectedSessions) {
 			if (session.isOpen())
-				session.getAsyncRemote().sendText(message);
+				session.getAsyncRemote().sendText(aMessage);
 		}
-		System.out.println("Message received: " + message);
+		System.out.println("Message received: " + aMessage);
 	}
 
 	@OnError
