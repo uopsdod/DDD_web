85ab9b669c2afacc371972192ee8b48a334b3ee6
uopsdod <uopsdod@gmail.com>
2016-11-17 19:55:11 +0800

    在chat.hbm.xml中新增一對多的set物件 (問題點:gson在轉換此set物件時會出現無限迴圈，暫時不會用到但要小心)

--- DDD_web/src/com/chat/model/ChatVO.java
+++ DDD_web/src/com/chat/model/ChatVO.java
@@ -3,10 +3,16 @@ package com.chat.model;
 import java.io.Serializable;
 import java.sql.Clob;
 import java.sql.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import com.memchat.model.MemChatVO;
+import com.ord.model.OrdVO;
 
 public class ChatVO implements Serializable {
 	private String chatId; // NOT NULL
 	private String chatName;
+	private Set<MemChatVO> chatMemChats = new LinkedHashSet<MemChatVO>();
 	
 	public String getChatId() {
 		return this.chatId;
@@ -20,5 +26,12 @@ public class ChatVO implements Serializable {
 	public void setChatName(String aChatName) {
 		this.chatName = aChatName;
 	}
+	public Set<MemChatVO> getChatMemChats() {
+		return chatMemChats;
+	}
+	public void setChatMemChats(Set<MemChatVO> chatMemChats) {
+		this.chatMemChats = chatMemChats;
+	}
+	
 	
 }
