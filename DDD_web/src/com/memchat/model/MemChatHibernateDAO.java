8f54e53f6875bc7abe67aa29a6e383fdcca8384d
uopsdod <uopsdod@gmail.com>
2016-11-18 01:43:24 +0800

    共住-歷史訊息getNewestMsgEachChatId方法STMT修正回來

--- DDD_web/src/com/memchat/model/MemChatHibernateDAO.java
+++ DDD_web/src/com/memchat/model/MemChatHibernateDAO.java
@@ -44,9 +44,9 @@ public class MemChatHibernateDAO implements MemChatDAO_interface {
 	private static final String GET_ALL_MEMCHATCHATID = "SELECT memChatChatId, memChatMemId, memChatDate, memChatContent, memChatPic, memChatStatus, memChatToMemId FROM memChat WHERE memChatChatId= ? ORDER BY memChatChatId";
 	private static final String GET_ALL_MSGBTWNTWOMEMS = "SELECT * FROM memchat WHERE memChatMemId IN (?,?) AND memChatToMemId IN (?,?) ORDER BY memChatDate";
 	private static final String GET_CHAT_IDBTWNTWOMEMS = "SELECT distinct memChatChatId FROM memchat WHERE memChatMemId IN (?,?) AND memChatToMemId IN (?,?)";
-	//private static final String GET_NEWESTMSG_CHATID = "SELECT * FROM memChat JOIN (SELECT memChatChatId as chatId_b, MAX(memChatDate) as date_b FROM memChat GROUP BY memChatChatId) ON memChatChatId = chatId_b AND memChatDate = date_b WHERE memChatMemId = ? OR memChatToMemId = ? ORDER BY memChatDate DESC";
+	private static final String GET_NEWESTMSG_CHATID = "SELECT * FROM memChat JOIN (SELECT memChatChatId as chatId_b, MAX(memChatDate) as date_b FROM memChat GROUP BY memChatChatId) ON memChatChatId = chatId_b AND memChatDate = date_b WHERE memChatMemId = ? OR memChatToMemId = ? ORDER BY memChatDate DESC";
 	//private static final String GET_NEWESTMSG_CHATID = "SELECT * FROM (SELECT * FROM memChat WHERE ROWID IN (SELECT MAX(ROWID) FROM memchat GROUP BY memChatChatId)) WHERE memChatMemId = ? OR memChatToMemId = ? ORDER BY memChatDate DESC";
-	private static final String GET_NEWESTMSG_CHATID = "SELECT * FROM memChat WHERE (memChatMemId = ? OR memChatToMemId = ?) AND ROWID IN (SELECT MAX(ROWID) FROM memchat GROUP BY memChatChatId) ORDER BY memChatDate DESC";
+	//private static final String GET_NEWESTMSG_CHATID = "SELECT * FROM memChat WHERE (memChatMemId = ? OR memChatToMemId = ?) AND ROWID IN (SELECT MAX(ROWID) FROM memchat GROUP BY memChatChatId) ORDER BY memChatDate DESC";
 	
 
 
