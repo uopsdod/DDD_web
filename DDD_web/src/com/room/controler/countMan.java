018518cb5b1146c1a5b670930e7edf633eaf0374
cucuboy <cucu31413@yahoo.com.tw>
2016-11-26 19:23:56 +0800

    關閉簡訊寄送 之後要打開, 修改QRcode內容

---
+++ DDD_web/src/com/room/controler/countMan.java
@@ -0,0 +1,34 @@
+package com.room.controler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class countMan
+ */
+@WebServlet("/countMan")
+public class countMan extends HttpServlet {
+
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		
+		
+	}	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+		
+		
+		
+	}
+
+}
